9a6f784a61af49a8debfb853fe7404a43dc3a6b7
Hamidreza Bahmani <hb.hamidrezabahmani.com@gmail.com>
2020-12-12 17:09:18 +0330

    Write Persian number

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ anElement = new AutoNumeric('#DEMO', {
     decimalPlaces: 0
 });
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function toFarsiNumber(n) {
     const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', ];
 
@@ -17,20 +21,39 @@ function toFarsiNumber(n) {
         )
         .join('');
 }
+
+
+function toPersianwithcomma(x) {
+    newarry = []
+    splitarry = x.toString().split('')
+    for (i = 0; i < splitarry.length; i++) {
+        if (Number.isInteger(parseInt(splitarry[i]))) {
+            newarry.push(toFarsiNumber(splitarry[i]))
+        } else {
+            splitarry[i] = ","
+            newarry.push(splitarry[i])
+        }
+    }
+    return newarry.join('')
+
+}
 var form = document.getElementById("check-form");
 form.onsubmit = function(e) {
     e.preventDefault();
     rawval = document.getElementById("DEMO").value
     num = rawval.replace(/\./g, "");
     console.log(num.toPersianLetter() + " ریال");
-    console.log(document.getElementById("pcal1").value)
+    console.log(document.getElementById("pcal1").value.toLocaleString('ar-EG'))
     console.log(document.getElementById("reciver").value)
 
 
     document.getElementById("price-val-sentence").innerHTML = num.toPersianLetter() + ' ریال'
-    document.getElementById("price-val-num").innerHTML = "/" + rawval + "/"
+    document.getElementById("price-val-num").innerHTML = "/" + toPersianwithcomma(rawval) + "/"
     document.getElementById("date-val").innerHTML = document.getElementById("pcal1").value
     document.getElementById("reciver-val").innerHTML = document.getElementById("reciver").value
     console.log(rawval)
     console.log(toFarsiNumber(rawval))
+    console.log(toPersianwithcomma(rawval))
+
+
 };
